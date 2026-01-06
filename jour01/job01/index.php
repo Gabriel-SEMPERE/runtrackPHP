396c1164c6406af97fea60cbd2111aaccc23935c
GabriemSEMP <gabriel.sempere@laplateforme.io>
2026-01-06 10:21:50 +0100

    job01 fini

---
+++ jour01/job01/index.php
@@ -0,0 +1,19 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+$val = 6; 
+$mybool = true;
+
+
+echo "$str2 $str $str3 "; 
+echo "$val ";
+var_dump($mybool);
+$mybool = false;
+$val = $val + 4; 
+echo "$val ";
+var_dump($mybool);
+
+
+
+?>
