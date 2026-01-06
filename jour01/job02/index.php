3d7a5073471a1f64d01f7143adb5c2ba377df8d0
GabriemSEMP <gabriel.sempere@laplateforme.io>
2026-01-06 10:52:25 +0100

    job02 et 03 fini

---
+++ jour01/job02/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// J'ai echo en php Hello Laplateforme!
+echo "Hello LaPlateforme!";
+
+# J'ai aussi echo Hello LaPlateforme!
+echo "Hello LaPlateforme!";
+
+?>
---
+++ jour01/job03/index.php
@@ -0,0 +1,27 @@
+<?php 
+$bool = true;
+$ent = 10;
+$nbr = 2.5;
+$cdc = "mot";
+
+echo 
+"
+<table> 
+<thead>
+<tr>
+<th> booleean </th>
+<th> caractère </th>
+<th> entier </th>
+<th> nombre </th>
+</thead>
+<tbody>
+<tr>
+<th> $bool </th>
+<td> $cdc </td>
+<td> $ent </td>
+<td> $nbr </td>
+</table>
+
+";
+
+?> 
